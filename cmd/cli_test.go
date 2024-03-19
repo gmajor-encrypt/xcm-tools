61ee07eb4833a15062193033331321a80685f53f
gmajor <gmajorencrypt@gmail.com>
2024-03-19 11:17:18 +0800

    Fix cli test

--- cmd/cli_test.go
+++ cmd/cli_test.go
@@ -8,10 +8,10 @@ import (
 func Test_Cli(t *testing.T) {
 	app := setup()
 	assert.NoError(t, app.Run([]string{"h"}))
-	assert.EqualValues(t, len(app.Commands), 4)
+	assert.EqualValues(t, len(app.Commands), 5)
 	assert.Equal(t, app.Name, "Xcm tools")
 
-	names := []string{"send", "parse", "tracker", "help"}
+	names := []string{"send", "parse", "tracker", "trackerBridge", "help"}
 	for i := 0; i < 4; i++ {
 		assert.Equal(t, app.Commands[i].Name, names[i])
 	}
